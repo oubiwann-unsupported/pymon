bf42bb5c5dbe51c622c294b5aca7e2c8e7a2763b
oubiwann <oubiwann@d7e4d4cf-ead8-0310-84d9-c13688e585a2>
2005-04-11 22:37:58 +0000

    sandbox: Added zodb schema creation script as well as the ZODB viewer script from ASPN.
    git-svn-id: https://pymon.svn.sourceforge.net/svnroot/pymon@108 d7e4d4cf-ead8-0310-84d9-c13688e585a2

--- sandbox/config_api/simple.dict.py
+++ sandbox/config_api/simple.dict.py
@@ -112,3 +112,33 @@ Rules:
 For each service: name it, get the defaults, build a list of hosts
 For each host: enable or disable escalation, build escalation levels
 For each escalation level: name it, add a maillist
+'''
+
+''' pprint.pprint(config) '''
+{'services': [{'defaults': {'command': 'ping',
+                            'error_threshold': (0, 25),
+                            'message_template': 'There was a %s%% ping return from host %s',
+                            'ok_threshold': (67, 100),
+                            'ping_binary': '/bin/ping',
+                            'ping_count': 4,
+                            'service_name': 'connectivity',
+                            'warn_threshold': (26, 66)},
+               'hosts': [{'escalation': {'enabled': True,
+                                         'groups': {'level0': ['oubiwann@myrealbox.com',
+                                                               'oubiwann@yahoo.com'],
+                                                    'level1': ['oubiwann@myrealbox.com']}},
+                          'name': 'shell1.adytum.us'},
+                         {'escalation': {'enabled': True,
+                                         'groups': {'level0': ['oubiwann@myrealbox.com',
+                                                               'oubiwann@yahoo.com'],
+                                                    'level1': ['oubiwann@myrealbox.com']}},
+                          'name': 'shell2.adytum.us'}],
+               'type': 'ping'},
+              {'hosts': None, 'type': 'http', 'defaults': None}],
+ 'system': {'database': {'directory': 'data/zodb',
+                         'host': 'localhost',
+                         'password': None,
+                         'port': None,
+                         'type': 'zodb',
+                         'user': None}}}
+
---
+++ sandbox/zodb/zodbschema.py
@@ -0,0 +1,85 @@
+from ZODB import FileStorage, DB
+from BTrees.OOBTree import OOBTree
+import sys
+sys.path.append('/usr/local/ZopeX3-3.0.0/lib/python/')
+from persistent import Persistent
+from pprint import pformat
+
+class ServiceRow(Persistent):
+    def __init__(self):
+        self.id = ''
+        self.name = ''
+        self.description = ''
+        self.organization = ''
+        self.service = ''
+        self.hostname = ''
+        self.count = 0
+        self.state_current = ''
+        self.state_last = ''
+        self.time_lastcheck = ''
+        self.time_lastok = ''
+        self.time_lastwarn = ''
+        self.time_lasterror = ''
+
+    def wx_str(self):
+        #return 'I want to paint his yoohoo... goOOoold.'
+        return pformat(self.__dict__)
+
+class ServiceTransitions(Persistent):
+    def __init__(self):
+        self.id = ''
+        self.organization = ''
+        self.hostname = ''
+        self.service = ''
+        self.transition_status_start = ''
+        self.transition_status_end = ''
+        self.transition_time_start = ''
+        self.transition_time_end = ''
+
+    def wx_str(self):
+        #return 'I want to paint his yoohoo... goOOoold.'
+        return pformat(self.__dict__)
+
+
+if __name__ == '__main__':
+    # setup the data store
+    storage = FileStorage.FileStorage('data/zodb/pymondb.fs')
+    db = DB(storage)
+    conn = db.open()
+    dbroot = conn.root()
+    state_table = 'monitoring_states'
+    transition_table = 'monitoring_transitions'
+
+    # setup the state table for usage/insert
+    dbroot[state_table] = OOBTree()
+    mondb = dbroot[state_table]
+
+    mon = ServiceRow()
+    mon.id = 'myserver.hostingcompany.com-httpd' 
+    mon.name = 'Apache Web Server'
+    mon.description = 'This is the staging Web Server Service for 16 low-volume clients'
+    mon.organization = 'My Special Client'
+    mon.service = 'httpd'
+    mon.hostname = 'myserver.hostingcompany.com'
+    mon.state_current = 'OK'
+    mon.state_last = 'OK'
+    mondb[mon.id] = mon
+    get_transaction().commit()
+
+    # setup the transition table for usage/insert
+    dbroot[transition_table] = OOBTree()
+    mondb = dbroot[transition_table]
+
+    trans = ServiceTransitions()
+    trans.id = 'myserver.hostingcompany.com-httpd-2005.04.11.15:58:32'
+    trans.organization = 'My Special Client'
+    trans.hostname = 'myserver.hostingcompany.com'
+    trans.service = 'httpd'
+    trans.transition_status_start = ''
+    trans.transition_status_end = ''
+    trans.transition_time_start = ''
+    trans.transition_time_end = ''
+    mondb[trans.id] = trans
+    get_transaction().commit()
+
+
