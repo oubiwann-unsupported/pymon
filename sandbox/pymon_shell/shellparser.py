ba75642a2eede47a1069b354d47ac7763ef20719
oubiwann <oubiwann@d7e4d4cf-ead8-0310-84d9-c13688e585a2>
2005-05-02 02:20:38 +0000

    There was a namespace collision between parser.py and something in distpatch, so I re-re-named parser.py to shellparser.py.
    git-svn-id: https://pymon.svn.sourceforge.net/svnroot/pymon@115 d7e4d4cf-ead8-0310-84d9-c13688e585a2

--- sandbox/pymon_shell/shellparser.py
+++ sandbox/pymon_shell/shellparser.py
@@ -5,9 +5,10 @@ import dispatch
 
 class ShellParser:
     '''
-    >>> p = ShellParser(
+    >>> p = ShellParser()
     >>> p.parse('show 192.168.0.1 http status')
     >>> print p.command()
+    ('192.168.0.1', 'http', 'show', 'status')
     '''
 
     def __init__(self):
@@ -17,31 +18,32 @@ class ShellParser:
         self.cmd_type = "";
 
     def parse(self, cmd):
+        # grammar stuff
         integer = Word(nums)
-        ip_address = Combine( integer + "." + integer + "." + integer + "." + integer )
+        ip_address = Combine(integer + "." + integer + "." + integer + "." + integer)
         cmd_start = CaselessLiteral("show")
         cmd_type = CaselessLiteral("status")
-        monitor = Word( alphas, alphanums)
-        cmd_pattern =  cmdStart.setResultsName("cmdAction") + ipAddress.setResultsName("ipAddr") + monitor.setResultsName("monitor") + cmdType.setResultsName("cmdType")
-        parsed_tuple = cmdPattern.scanString(cmd)
+        monitor = Word(alphas, alphanums)
 
-        for srvr, startloc, endloc in parsed_tuple:
+        # parsing stuff
+        cmd_pattern =  cmd_start.setResultsName("cmd_action") + ip_address.setResultsName("ip_addr") + monitor.setResultsName("monitor") + cmd_type.setResultsName("cmd_type")
+        for srvr, startloc, endloc in cmd_pattern.scanString(cmd):
             self.ip_addr = srvr.ip_addr
             self.monitor = srvr.monitor
             self.cmd_action = srvr.cmd_action
             self.cmd_type = srvr.cmd_type
 
-    [dispatch.generic()]
+    [ dispatch.generic() ]
     def command(self):
         """generic method for cmd"""
 
-    [command.when("self.cmd_type == 'status'")]
+    [ command.when("self.cmd_type == 'status'") ]
     def commandStatus(self):
         return self.ip_addr, self.monitor, self.cmd_action, self.cmd_type
 
 def _test():
-    import doctest, parser
-    doctest.testmod(parser)
+    import doctest, shellparser
+    doctest.testmod(shellparser)
 
 if __name__ == '__main__':
     _test()
