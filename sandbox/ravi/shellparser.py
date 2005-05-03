9cb7858548769ad8bb5b73a4ca9c9711c6e15f93
ravi <ravi@d7e4d4cf-ead8-0310-84d9-c13688e585a2>
2005-05-03 10:52:27 +0000

    added more commands
    git-svn-id: https://pymon.svn.sourceforge.net/svnroot/pymon@121 d7e4d4cf-ead8-0310-84d9-c13688e585a2

--- sandbox/ravi/shellparser.py
+++ sandbox/ravi/shellparser.py
@@ -1,6 +1,8 @@
 from pyparsing import Word, Combine, Suppress, CharsNotIn, nums
 from pyparsing import alphas, alphanums
 from pyparsing import CaselessLiteral
+from pyparsing import Optional
+from pyparsing import Forward
 import dispatch
 
 class ShellParser:
@@ -8,7 +10,13 @@ class ShellParser:
     >>> p = ShellParser()
     >>> p.parse('show 192.168.0.1 http status')
     >>> print p.command()
-    ('192.168.0.1', 'http', 'show', 'status')
+    ('192.168.0.1', (['http'], {}), 'show', 'status')
+    >>> p.parse('show 192.168.0.1 http summary')
+    >>> print p.command()
+    ('192.168.0.1', (['http'], {}), 'show', 'summary')
+    >>> p.parse('show 192.168.0.1 * summary')
+    >>> print p.command()
+    ('192.168.0.1', (['*'], {}), 'show', 'summary')
     '''
 
     def __init__(self):
@@ -36,16 +44,42 @@ class ShellParser:
         monitor = Word(alphas, alphanums)
 
         # parsing stuff
-        cmd_pattern =  cmd_start.setResultsName("cmd_action") \
+        cmd_pattern = Forward()
+        cmd_pattern <<  ( cmd_start.setResultsName("cmd_action") \
             + ip_address.setResultsName("ip_addr") \
-            + monitor.setResultsName("monitor") \
-            + cmd_type.setResultsName("cmd_type")
-        for srvr, startloc, endloc in cmd_pattern.scanString(self.cmd):
-            ip_addr = srvr.ip_addr
-            monitor = srvr.monitor
-            cmd_action = srvr.cmd_action
-            cmd_type = srvr.cmd_type
+            + (monitor | "*").setResultsName("monitor") \
+            + cmd_type.setResultsName("cmd_type") )
+        tokens = cmd_pattern.parseString(self.cmd)
+        ip_addr = tokens.ip_addr
+        monitor = tokens.monitor
+        cmd_action = tokens.cmd_action
+        cmd_type = tokens.cmd_type
         return ip_addr, monitor, cmd_action, cmd_type
+
+
+    [ command.when("self.cmd_type == 'summary'") ]
+    def commandSummary(self):
+        integer = Word(nums)
+        ip_address = Combine(integer + "." +
+            integer + "." + integer + "." +
+            integer)
+        cmd_start = CaselessLiteral("show")
+        cmd_type = CaselessLiteral("summary")
+        monitor = Word(alphas, alphanums)
+
+        # parsing stuff
+        cmd_pattern = Forward()
+        cmd_pattern <<  ( cmd_start.setResultsName("cmd_action") \
+            + ip_address.setResultsName("ip_addr") \
+            + (monitor | "*").setResultsName("monitor") \
+            + cmd_type.setResultsName("cmd_type") )
+        tokens = cmd_pattern.parseString(self.cmd)
+        ip_addr = tokens.ip_addr
+        monitor = tokens.monitor
+        cmd_action = tokens.cmd_action
+        cmd_type = tokens.cmd_type
+        return ip_addr, monitor, cmd_action, cmd_type
+
 
 def _test():
     import doctest, shellparser
