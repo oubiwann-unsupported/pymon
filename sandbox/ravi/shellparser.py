e34c62543f77458d64ccffb5464b29044f0eb292
ravi <ravi@d7e4d4cf-ead8-0310-84d9-c13688e585a2>
2005-05-22 18:39:46 +0000

    added show escalations and help
    git-svn-id: https://pymon.svn.sourceforge.net/svnroot/pymon@146 d7e4d4cf-ead8-0310-84d9-c13688e585a2

--- sandbox/ravi/shellparser.py
+++ sandbox/ravi/shellparser.py
@@ -11,12 +11,21 @@ class ShellParser:
     >>> p.parse('show 192.168.0.1 http status')
     >>> print p.command()
     ('192.168.0.1', (['http'], {}), 'show', 'status')
-    >>> p.parse('show 192.168.0.1 http summary')
+    >>> p.parse('show 192.168.0.1 http config')
     >>> print p.command()
-    ('192.168.0.1', (['http'], {}), 'show', 'summary')
-    >>> p.parse('show 192.168.0.1 * summary')
+    ('192.168.0.1', (['http'], {}), 'show', 'config')
+    >>> p.parse('show 192.168.0.1 * config')
     >>> print p.command()
-    ('192.168.0.1', (['*'], {}), 'show', 'summary')
+    ('192.168.0.1', (['*'], {}), 'show', 'config')
+    >>> p.parse('show * escalations')
+    >>> p.command()
+    ((['*'], {}), 'show', 'escalations')
+    >>> p.parse('show 192.156.5.5 escalations')
+    >>> p.command()
+    ((['192.156.5.5'], {}), 'show', 'escalations')
+    >>> p.parse('help')
+    >>> p.command()
+    ['help']
     '''
 
     def __init__(self):
@@ -31,6 +40,11 @@ class ShellParser:
     [ dispatch.generic() ]
     def command(self):
         """generic method for cmd"""
+
+    [ command.when("self.cmd_type == 'help' or self.cmd_type == '?'") ]
+    def commandHelp(self):
+        cmd_list = ['help']
+        return cmd_list
 
     [ command.when("self.cmd_type == 'status'") ]
     def commandStatus(self):
@@ -57,14 +71,14 @@ class ShellParser:
         return ip_addr, monitor, cmd_action, cmd_type
 
 
-    [ command.when("self.cmd_type == 'summary'") ]
-    def commandSummary(self):
+    [ command.when("self.cmd_type == 'config'") ]
+    def commandConfig(self):
         integer = Word(nums)
         ip_address = Combine(integer + "." +
             integer + "." + integer + "." +
             integer)
         cmd_start = CaselessLiteral("show")
-        cmd_type = CaselessLiteral("summary")
+        cmd_type = CaselessLiteral("config")
         monitor = Word(alphas, alphanums)
 
         # parsing stuff
@@ -80,6 +94,23 @@ class ShellParser:
         cmd_type = tokens.cmd_type
         return ip_addr, monitor, cmd_action, cmd_type
 
+    [ command.when("self.cmd_type == 'escalations'") ]
+    def commandConfig(self):
+        integer = Word(nums)
+        ip_address = Combine(integer + "." +
+            integer + "." + integer + "." +
+            integer)
+        cmd_start = CaselessLiteral("show")
+        cmd_type = CaselessLiteral("escalations")
+        cmd_pattern = Forward()
+        cmd_pattern <<  ( cmd_start.setResultsName("cmd_action") \
+            + (ip_address | "*").setResultsName("ip_addr") \
+            + cmd_type.setResultsName("cmd_type") )
+        tokens = cmd_pattern.parseString(self.cmd)
+        ip_addr = tokens.ip_addr
+        cmd_action = tokens.cmd_action
+        cmd_type = tokens.cmd_type
+        return ip_addr, cmd_action, cmd_type
 
 def _test():
     import doctest, shellparser
