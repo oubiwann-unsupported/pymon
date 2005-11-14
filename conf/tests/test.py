fc46df0bd6f076c3aa3a47d87caa73fc4581c930
oubiwann <oubiwann@d7e4d4cf-ead8-0310-84d9-c13688e585a2>
2005-11-14 04:43:13 +0000

    * Added support for default email notification list withing the general configuration section and overrides in ping-defaults as well as ping. * Added another set of tests for configuration (for pings).
    git-svn-id: https://pymon.svn.sourceforge.net/svnroot/pymon@224 d7e4d4cf-ead8-0310-84d9-c13688e585a2

--- conf/tests/test.py
+++ conf/tests/test.py
@@ -35,6 +35,45 @@
 3
 >>> ex2.state_definitions.error
 4
+
+# load example 3
+>>> schema = ZConfig.loadSchema('example3.xml')
+>>> ex3, nil = ZConfig.loadConfig(schema, 'example3.conf')
+
+# test values
+>>> ex3.services.ping_defaults.count
+4
+>>> ex3.services.ping_defaults.interval
+25
+>>> ex3.services.ping_defaults.binary
+'/sbin/ping'
+>>> enabled = [ x.uri for x in ex3.services.pings if x.check_enabled ]
+>>> enabled.sort()
+>>> enabled
+['shell1.adytum.us', 'svn.adytum.us']
+>>> ex3.services.ping_defaults.message_template % (100, 'localhost.localdomain')
+'There was a 100% ping return from host localhost.localdomain'
+>>> ex3.services.ping_defaults.error_message % ('localhost.localdomain', 2)
+'pymon ERROR: localhost.localdomain : 2% loss'
+>>> emails = ex3.notification_list.emails
+>>> emails.sort()
+>>> emails
+['jojo.idiot@circus_boy.com', 'pretty@new_pet.com']
+>>> emails = ex3.services.ping_defaults.notification_list_append.emails
+>>> emails.sort()
+>>> emails
+['living_in_a_van@down-by.the-river.com']
+>>> for ping in ex3.services.pings:
+...   try:
+...     emails = ping.notification_list_replace.emails
+...     emails.sort()
+...     emails
+...   except AttributeError:
+...     print "No replace list..."
+No replace list...
+No replace list...
+['bull@butchers-ass.com', 'thick@candy-shell.com']
+
 '''
 
 def _test():
--- lib/pymon/zconfig.py
+++ lib/pymon/zconfig.py
@@ -26,3 +26,16 @@ def checkBy(value):
     legal_values = ['services', 'groups']
     validator = ValidateCaseInsensitiveList(legal_values)
     return validator.validate(value)
+
+def rangedValues(range_string):
+    '''
+    Validator human-readable, easily/commonly understood format 
+    representing ranges of values. At it's most basic, the following 
+    is a good example:
+        25-30
+    A more complicated range value would be something like:
+        20-25, 27, 29, 31-33
+    '''
+    # XXX need to write a regular expression that really checks for this
+    return range_string
+    
