fd39b1fd0f77cf80d9813044043b080a229b3d3a
oubiwann <oubiwann@d7e4d4cf-ead8-0310-84d9-c13688e585a2>
2008-01-09 03:35:00 +0000

    2008.01.08
    * Added best practices/advice to usage in discoverServices script.
    * Coped the ping plugin as the basis for the new ping_tcp plugin.
    
    
    
    git-svn-id: https://pymon.svn.sourceforge.net/svnroot/pymon@359 d7e4d4cf-ead8-0310-84d9-c13688e585a2

---
+++ plugins/ping_tcp/client.py
@@ -0,0 +1,69 @@
+from twisted.spread import pb
+from twisted.internet.protocol import Protocol
+
+from pymon.utils.logger import log
+from pymon.clients import ClientMixin
+from pymon.utils.pingparser import OutputParser
+
+
+class TCPPingClient(Protocol, ClientMixin):
+
+    def connectionMade(self):
+        self.factory.deferred.callback("success")
+        self.transport.loseConnection()
+        ClientMixin.connectionMade(self)
+
+    def connectionLost(self, reason):
+        results = self.factory.data
+        log.debug(results)
+        # XXX this needs to be done in workflow
+        self.rules.check(results['gain'])
+        self.rules.setMsg(results['gain'], self.getHost())
+        self.rules.setSubj(self.getHost(), results['loss'])
+        if self.rules.isSendMessage():
+            self.rules.sendIt()
+
+        # dump info to log file
+        log.debug('Service: %s' % self.factory.uid)
+        log.info(self.rules.msg)
+        log.info(self.rules.subj)
+        log.debug("Status: %s for %s" % (self.rules.status, host))
+
+        # update state information
+        self.updateState()
+
+        # dump info to log file
+        log.debug('State Data: '+str(self.factory.state.data)+'\n')
+
+class LocalAgentPingClient(pb.Broker, ClientMixin):
+
+    def connectionMade(self):
+        pb.Broker.connectionMade(self)
+        ClientMixin.connectionMade(self)
+
+    def connectionLost(self, reason):
+        # parse returned data
+        log.debug(self.factory.data)
+        parse = OutputParser(self.factory.data)
+        loss = parse.getPingLoss()
+        gain = parse.getPingGain()
+        host = self.getHost()
+
+        # push the returned data through the threshold checks
+        status = self.rules.check(gain)
+        self.workflow.checkTransition(status, self.factory.cfg)
+        #self.rules.setMsg(gain, host)
+        #self.rules.setSubj(host, loss)
+        #if self.rules.isSendMessage():
+        #    self.rules.sendIt()
+
+        # update state information
+        self.updateState()
+
+        # dump info to log file
+        log.info('State Data: '+str(self.factory.state.data)+'\n')
+
+        # final cleanup
+        ClientMixin.connectionLost(self)
+
+
---
+++ plugins/ping_tcp/monitor.py
@@ -0,0 +1,58 @@
+from twisted.python import components
+from twisted.spread import pb
+from twisted.internet.protocol import ClientFactory
+
+from pymon.utils.logger import log
+from pymon.interfaces import IState
+from pymon.application import MonitorState
+from pymon.application import globalRegistry
+from pymon.monitors import BaseMonitor
+
+from client import LocalAgentPingClient
+
+class LocalAgentPingMonitor(pb.PBClientFactory, BaseMonitor):
+
+    protocol = LocalAgentPingClient
+
+    def __init__(self, uid, cfg):
+        pb.PBClientFactory.__init__(self)
+        BaseMonitor.__init__(self, uid, cfg)
+
+        # get the info in order to make the next ping
+        self.binary = self.cfg.defaults.binary
+        count = '-c %s' % self.cfg.defaults.count
+        self.args = [count, self.host]
+        port = int(cfg.app.agents.port)
+        self.reactor_params = ('127.0.0.1', port, self)
+
+    def __call__(self):
+        BaseMonitor.__call__(self)
+        d = self.getRootObject()
+        d.addCallback(self.pingHost)
+        d.addErrback(log.error)
+        d.addCallback(self.getPingReturn)
+        d.addErrback(log.error)
+
+    def pingHost(self, pbobject):
+        return pbobject.callRemote('call', self.binary, self.args)
+
+    def getPingReturn(self, results):
+        self.data = results
+        #print dir(self)
+        log.debug('Ping results: %s' % results)
+        self.disconnect()
+
+    def clientConnectionLost(self, connector, reason, reconnecting=1):
+        """
+        Reconnecting subclasses should call with reconnecting=1.
+        """
+        if reconnecting:
+            # any pending requests will go to next connection attempt
+            # so we don't fail them.
+            self._broker = None
+            self._root = None
+        else:
+            self._failAll(reason)
+
+components.registerAdapter(MonitorState, LocalAgentPingMonitor, IState)
+
