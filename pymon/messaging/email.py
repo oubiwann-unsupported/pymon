7262fde8ed4afe703664637c9be82449d98b77bd
oubiwann <oubiwann@d7e4d4cf-ead8-0310-84d9-c13688e585a2>
2008-01-25 23:31:16 +0000

    * Moved old, unnamed messaging class out of workflow.rules and into messaging.email.
    git-svn-id: https://pymon.svn.sourceforge.net/svnroot/pymon@382 d7e4d4cf-ead8-0310-84d9-c13688e585a2

--- pymon/messaging/email.py
+++ pymon/messaging/email.py
@@ -1,3 +1,7 @@
+from pymon.config import cfg
+from pymon.utils import isInList, isInRange, isExactly
+from pymon.utils.logger import log
+
 class LocalMail(Email):
 
     def setSendmailBinary(self, bin='/usr/sbin/sendmail'):
@@ -24,4 +28,73 @@ class RemoteMail(Email):
         mail=smtplib.SMTP(self.host)
         mail.sendmail(self.frm,self.to,self.mail_data)
         mail.close()
- 
+
+class AsYetUndterminedClassName(object):
+    # XXX define me!
+
+    # XXX what about escalation? Which messages are sent? How are
+    # messages sent? When are they NOT sent? Who gets what?
+    def isMessage(self):
+        if self.status == self.factory.cfg.app.state_definitions.ok:
+            return False
+        return True
+
+    def msgEnabled(self):
+        if self.factory.cfg.notifications.enabled:
+            return True
+        return False
+
+    def cutOffReached(self):
+        cutoff = self.factory.cfg.notifications.cut_off
+        if self.factory.state.get('count') > cutoff:
+            log.info("Incident count has passed the cut-off " + \
+                "threshold; not sending email.")
+            return True
+        return False
+
+    def isSendMessage(self):
+        if (self.isMessage and self.msgEnabled
+            and not self.cutOffReached):
+            return True
+        return False
+
+    def setMsg(self, *args):
+        self.msg = self.factory.cfg.defaults.message_template % args
+
+    def setSubj(self, *args):
+        status = cfg.getStateNameFromNumber(self.status)
+        if status == 'unknown':
+            self.subj = "Unknown status"
+        else:
+            msg = getattr(self.factory.cfg.defaults, '%s_message' % status)
+            # XXX This try/except is a quick hack that REALLY needs to
+            # be done right... just because it's so ugly... and...
+            # ugly.
+            try:
+                self.subj = msg % args
+            except TypeError:
+                # for "not all arguments converted during string
+                # formatting"
+                self.subj = msg % args[0]
+
+    def sendIt(self):
+        if self.status == self.factory.cfg.app.state_definitions.recovering:
+            status_id = self.factory.state.get('current status')
+            status = cfg.getStateNameFromNumber(status_id)
+            self.msg = self.msg + "\r\nRecovering from '%s'." % status
+        sendmail = self.factory.cfg.sendmail
+        frm = self.factory.cfg.mail_from
+        # XXX we probably want to make the actual sending of emails
+        # non-blocking. Dererreds anyone?
+        # XXX modify this when support for escalation and different
+        # group levels is added to python
+        for address in cfg.getMailList(self.factory.uid):
+            email = LocalMail()
+            email.setSendmailBinary(sendmail)
+            email.setSubject(self.subj)
+            email.setTo(address)
+            email.setFrom(frm)
+            email.setData(self.msg)
+            email.send()
+            log.info(self.factory.cfg.defaults.sent_message % address)
+
